ed09560fa167d9fe1e7692fa9021be90c029304b
cetecom <cetecom@LCV11019>
2019-04-02 15:12:16 -0300

    1 sesion

---
+++ css/estilo.php
@@ -0,0 +1,28 @@
+
+
+a.submenu{
+    padding: 0 0 0 40px;
+    display: none;
+}
+
+#contenedor{
+    width:100%;
+}
+#titulo{
+    width:100%;
+    height: 80px;
+    float: left;
+	background: red;
+}
+#menu{
+    width: 30%;
+    height: 500px;
+    float: left;
+	background: green;
+}
+#contenido{
+   width: 70%;
+   height: 500px; 
+   float: left;
+   background: yellow;
+}
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include('lib/constantes.php')?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link href="css/estilo.css" rel="stylesheet" type="text/css"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+    <body>
+        
+        <div id="contenedor">
+            <div id="titulo"></div>
+            <div id="menu"><?php include('menu.php');?></div>
+            <div id="contenido"></div>
+        </div>
+        
+    </body>
+</html>
